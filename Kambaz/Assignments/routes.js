90ba91b0a0b7398c9922095901a6328ed24df9fb
jackiedeieso <deieso.j@northeastern.edu>
2025-04-02 18:47:21 -0400

    assignments error

--- Kambaz/Assignments/routes.js
+++ Kambaz/Assignments/routes.js
@@ -1,29 +1,29 @@
 import * as dao from "./dao.js";
 
 export default function AssignmentRoutes(app) {
-  app.post("/api/assignments", (req, res) => {
-    const newAssignment = dao.createAssignment(req.body);
+  app.post("/api/assignments", async (req, res) => {
+    const newAssignment = await dao.createAssignment(req.body);
     res.json(newAssignment);
   });
 
-  app.get("/api/assignments", (req, res) => {
-    const assignments = dao.findAllAssignments();
+  app.get("/api/assignments", async (req, res) => {
+    const assignments = await dao.findAllAssignments();
     res.json(assignments);
   });
 
-  app.get("/api/assignments/:aid", (req, res) => {
-    const assignment = dao.findAssignmentById(req.params.aid);
+  app.get("/api/assignments/:aid", async (req, res) => {
+    const assignment = await dao.findAssignmentById(req.params.aid);
     if (assignment) res.json(assignment);
     else res.sendStatus(404);
   });
 
-  app.put("/api/assignments/:aid", (req, res) => {
-    const updated = dao.updateAssignment(req.params.aid, req.body);
+  app.put("/api/assignments/:aid", async (req, res) => {
+    const updated = await dao.updateAssignment(req.params.aid, req.body);
     res.json(updated);
   });
 
-  app.delete("/api/assignments/:aid", (req, res) => {
-    dao.deleteAssignment(req.params.aid);
+  app.delete("/api/assignments/:aid", async (req, res) => {
+    await dao.deleteAssignment(req.params.aid);
     res.sendStatus(204);
   });
 }
