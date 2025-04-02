206584791d95438cb65f7f749bb3902f930ff273
jackiedeieso <deieso.j@northeastern.edu>
2025-04-02 17:32:06 -0400

    fixing netlify url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  origin: process.env.NETLIFY_URL || "https://jackie-deiso-kambaz.netlify.app",
 }));
 
 app.use(express.json());
